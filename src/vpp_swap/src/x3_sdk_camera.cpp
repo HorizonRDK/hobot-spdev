dbf1d228d0118e7cc05ee25bdb2d7ca45694cb5f
luojiale <jiale.luo@horizon.ai>
2023-06-14 16:27:59 +0800

    fix(spdev:vpp_swap) Adjust the judgment condition of VPS channel
    Summary:
    	1. Fixed an issue where the VPS channel would be incorrectly selected when the resolution was zoomed in on one side
    	2. When the height and width do not meet the alignment requirements, an error message will be printed and exited
    
    Signed-off-by: luojiale <jiale.luo@horizon.ai>

--- src/vpp_swap/src/x3_sdk_camera.cpp
+++ src/vpp_swap/src/x3_sdk_camera.cpp
@@ -26,7 +26,7 @@ namespace srpy_cam
 
 static int vps_select_chn(int *chn_en, int src_width, int src_height, int dst_width, int dst_height)
 {
-    if (((dst_width <= src_width) || (dst_height <= src_height)) &&
+    if (((dst_width <= src_width) && (dst_height <= src_height)) &&
         (!(*chn_en & 1 << HB_VIO_IPU_DS2_DATA)) &&
         (dst_width <= 4096 && dst_height <= 4096)) {
         return HB_VIO_IPU_DS2_DATA;
@@ -46,7 +46,7 @@ static int vps_select_chn(int *chn_en, int src_width, int src_height, int dst_wi
             return HB_VIO_IPU_DS4_DATA;
         }
     }
-    if (((dst_width >= src_width) || (dst_height > src_height)) &&
+    if (((dst_width >= src_width) && (dst_height >= src_height)) &&
             (!(*chn_en & 1 << HB_VIO_IPU_US_DATA)) &&
             (dst_width <= 4096 && dst_height <= 4096)) {
         return HB_VIO_IPU_US_DATA;
@@ -132,6 +132,12 @@ int x3_cam_init_param(x3_modules_info_t *info, const int pipe_id, const int vide
     // 2.3 配置group每个通道的参数
     info->m_vps_infos.m_vps_info[0].m_chn_num = chn_num;
     for (int i = 0; i < chn_num; i++) {
+        if((width[i] % 4 != 0) || (height[i] % 2 != 0))
+        {
+            LOGE_print("Width: %d must be divisible by 4, height: %d must be even!\n",width[i],height[i]);
+            ret = -1;
+            break;
+        }
         chn_data = -1;
         if ((width[i] == 0) && (height[i] == 0)) {//如果高宽为0，那么就开一个和mipi原始高宽一致的通道
             width[i] = mipi_width;
